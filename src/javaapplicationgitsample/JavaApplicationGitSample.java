b0187b0c8dcfc2adf0d3eb391e8d98f0baf5d09d
ivy <ijadeleon@gmail.com>
2014-06-17 08:57:24 +0800

    modified: chuchu class

--- src/javaapplicationgitsample/JavaApplicationGitSample.java
+++ src/javaapplicationgitsample/JavaApplicationGitSample.java
@@ -15,5 +15,6 @@ public class JavaApplicationGitSample {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("chuchu...");
     }
 }
